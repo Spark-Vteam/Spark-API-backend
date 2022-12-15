ec3529ea6429d6ee07b5aa8e0c87d7d2a40f1798
John Fredriksson <jofr21@student.bth.se>
2022-12-15 09:56:51 +0100

    fixed status in admin mock data

--- Spark-mysql/sql/insert-1-admins.sql
+++ Spark-mysql/sql/insert-1-admins.sql
@@ -3,5 +3,5 @@ INSERT INTO Admins
     ( FirstName, LastName, PhoneNumber, EmailAdress, Authority, Password )
 VALUES
     ('Emmie', 'Service', '123-1337', 'emmie@oauth-guro.se', 10, 'password'),
-    ('Frida', 'Webadmin', '456-1337', 'frida@hAPI.se', 10, 'password'),
-    ('Erik', 'Masteradmin', '789-1337', 'erik@expo-experto.se', 10, 'password');
+    ('Frida', 'Webadmin', '456-1337', 'frida@hAPI.se', 20, 'password'),
+    ('Erik', 'Masteradmin', '789-1337', 'erik@expo-experto.se', 30, 'password');
