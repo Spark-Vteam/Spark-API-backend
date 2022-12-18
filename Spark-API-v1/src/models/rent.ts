58b135f95131beee9547c7db91cdac48949d18b9
fridavbg <fperssontech@gmail.com>
2022-12-18 15:47:38 +0100

    Updated /rent route

--- Spark-API-v1/src/models/rent.ts
+++ Spark-API-v1/src/models/rent.ts
@@ -1,54 +1,93 @@
 import database from '../db/db';
+import { NextFunction, Response } from 'express';
+import { FieldPacket, RowDataPacket } from 'mysql2/promise';
 
 const rentModel = {
-    showAllRents: async function showAllRents() {
+    /**
+     * Function to show all rents
+     * @async
+     * @returns {RowDataPacket} Resultset from the query.
+     */
+    showAllRents: async function showAllRents(res: Response, next: NextFunction) {
         const db = await database.getDb();
         try {
             const sql = `CALL get_rents()`;
 
-            const res = await db.query(sql);
+            const res: [RowDataPacket[], FieldPacket[]] = await db.query(sql);
 
-            return res[0];
+            return res[0][0];
+        } catch (error: any) {
+            next(res.status(404).send(error));
         } finally {
             await db.end();
         }
     },
-    getOneRent: async function getOneRent(rentId: string) {
+    /**
+     * Function to show one rent
+     * @async
+     * @returns {RowDataPacket} Resultset from the query.
+     */
+    getOneRent: async function getOneRent(rentId: string, res: Response, next: NextFunction) {
         const db = await database.getDb();
         try {
             const sql = `CALL get_rent(?)`;
-            const res = await db.query(sql, [rentId]);
-            return res[0];
+
+            const res: [RowDataPacket[], FieldPacket[]] = await db.query(sql, [rentId]);
+
+            return res[0][0];
+        } catch (error: any) {
+            next(res.status(404).send(error));
         } finally {
             await db.end();
         }
     },
-    getRentsByUserId: async function getRentsByUserId(userId: string) {
+    /**
+     * Function to show all rents for one User
+     * @async
+     * @returns {RowDataPacket} Resultset from the query.
+     */
+    getRentsByUserId: async function getRentsByUserId(userId: string, res: Response, next: NextFunction) {
         const db = await database.getDb();
         try {
             const sql = `CALL get_rents_by_user(?)`;
-            const res = await db.query(sql, [userId]);
-            return res[0];
+            const res: [RowDataPacket[], FieldPacket[]] = await db.query(sql, [userId]);
+            return res[0][0];
+        } catch (error: any) {
+            next(res.status(404).send(error));
         } finally {
             await db.end();
         }
     },
-    createOneRent: async function createOneRent(userId: string, bikeId: string) {
+    /**
+     * Function to create one rent
+     * @async
+     * @returns {RowDataPacket} Resultset from the query.
+     */
+    createOneRent: async function createOneRent(userId: string, bikeId: string, res: Response, next: NextFunction) {
         const db = await database.getDb();
         try {
             const sql = `CALL create_rent(?, ?)`;
-            const res = await db.query(sql, [userId, bikeId]);
-            return res[0];
+            const res: [RowDataPacket[], FieldPacket[]] = await db.query(sql, [userId, bikeId]);
+            return res[0][0];
+        } catch (error: any) {
+            next(res.status(404).send(error));
         } finally {
             await db.end();
         }
     },
-    updateOneRent: async function createOneRent(rentId: string) {
+    /**
+     * Function to update one rent
+     * @async
+     * @returns {RowDataPacket} Resultset from the query.
+     */
+    updateOneRent: async function createOneRent(rentId: string, res: Response, next: NextFunction) {
         const db = await database.getDb();
         try {
             const sql = `CALL update_rent(?)`;
-            const res = await db.query(sql, [rentId]);
-            return res[0];
+            const res: [RowDataPacket[], FieldPacket[]] = await db.query(sql, [rentId]);
+            return res[0][0];
+        } catch (error: any) {
+            next(res.status(404).send(error));
         } finally {
             await db.end();
         }
--- Spark-API-v1/src/routes/rent.ts
+++ Spark-API-v1/src/routes/rent.ts
@@ -1,4 +1,4 @@
-import { Request, Response, Router } from 'express';
+import { Request, Response, NextFunction, Router } from 'express';
 
 import rentModel from '../models/rent';
 const router = Router();
@@ -25,17 +25,10 @@ interface RentInfo {
  *
  * @returns {Response}
  */
-router.get('/rent', async (req: Request, res: Response) => {
-    try {
-        const allRents = await rentModel.showAllRents();
-        const allRentsData = JSON.parse(JSON.stringify(allRents));
-        if (allRentsData[0].length === 0) {
-            return res.status(404).send('No rents currently in the system');
-        }
-        return res.status(200).send(allRents);
-    } catch (error) {
-        return res.status(404).send(error);
-    }
+router.get('/rent', async (req: Request, res: Response, next: NextFunction) => {
+    const allRents = await rentModel.showAllRents(res, next);
+
+    return res.status(200).send({ success: true, data: allRents });
 });
 
 /**
@@ -50,17 +43,10 @@ router.get('/rent', async (req: Request, res: Response) => {
  *
  * @returns {Response}
  */
-router.get('/rent/:id', async (req: Request, res: Response) => {
-    try {
-        const oneRent = await rentModel.getOneRent(req.params.id);
-        const oneRentData = JSON.parse(JSON.stringify(oneRent));
-        if (oneRentData[0].length === 0) {
-            return res.status(404).send(`No rent with Id ${req.params.id} was found`);
-        }
-        return res.status(200).send(oneRent);
-    } catch (error) {
-        return res.status(404).send(error);
-    }
+router.get('/rent/:id', async (req: Request, res: Response, next: NextFunction) => {
+    const rentId = req.params.id;
+    const oneRent = await rentModel.getOneRent(rentId, res, next);
+    return res.status(200).send({ success: true, data: oneRent });
 });
 
 /**
@@ -75,19 +61,11 @@ router.get('/rent/:id', async (req: Request, res: Response) => {
  *
  * @returns {Response}
  */
-router.get('/rent/user/:id', async (req: Request, res: Response) => {
-    try {
-        const rentByUserId = await rentModel.getRentsByUserId(req.params.id);
+router.get('/rent/user/:id', async (req: Request, res: Response, next: NextFunction) => {
+    const userId = req.params.id;
+    const rentByUserId = await rentModel.getRentsByUserId(userId, res, next);
 
-        const rentByUserIdData = JSON.parse(JSON.stringify(rentByUserId));
-
-        if (rentByUserIdData[0].length === 0) {
-            return res.status(404).send(`No rents for user Id ${req.params.id} was found`);
-        }
-        return res.status(200).send(rentByUserId);
-    } catch (error) {
-        return res.status(404).send(error);
-    }
+    return res.status(200).send(rentByUserId);
 });
 
 /**
@@ -102,16 +80,13 @@ router.get('/rent/user/:id', async (req: Request, res: Response) => {
  *
  * @returns {Response}
  */
-router.post('/rent/user/:id', async (req: Request, res: Response) => {
+router.post('/rent/user/:id', async (req: Request, res: Response, next: NextFunction) => {
     const bikeId = req.body.bikeId;
     const userId = req.params.id;
-    try {
-        const newRent = await rentModel.createOneRent(userId, bikeId);
 
-        return res.status(200).send(`New rent created`);
-    } catch (error) {
-        return res.status(404).send(error);
-    }
+    await rentModel.createOneRent(userId, bikeId, res, next);
+
+    return res.status(200).send({ success: true, msg: `New rent created for user ${userId}` });
 });
 
 /**
@@ -126,15 +101,12 @@ router.post('/rent/user/:id', async (req: Request, res: Response) => {
  *
  * @returns {Response}
  */
-router.put('/rent/:id', async (req: Request, res: Response) => {
+router.put('/rent/:id', async (req: Request, res: Response, next: NextFunction) => {
     const rentId = req.params.id;
-    try {
-        const updateRent = await rentModel.updateOneRent(rentId);
 
-        return res.status(200).send(`Rent with id ${rentId} has been updated`);
-    } catch (error) {
-        return res.status(404).send(error);
-    }
+    await rentModel.updateOneRent(rentId, res, next);
+
+    return res.status(200).send({ success: true, msg: `Rent with id ${rentId} has been updated` });
 });
 
 export default router;
