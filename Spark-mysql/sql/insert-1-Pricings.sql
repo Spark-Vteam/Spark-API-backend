63c796f58e5051c31f3e84abe37441edfef1186c
John Fredriksson <jofr21@student.bth.se>
2022-12-21 14:39:47 +0100

    added mock-data for pricing

---
+++ Spark-mysql/sql/insert-1-Pricings.sql
@@ -0,0 +1,5 @@
+USE mydb
+INSERT INTO Pricings
+    ( Type, Description, Start, Minute, Parking, DiscountStartFree, DiscountEndParkingZone, DiscountEndCharging )
+VALUES
+    ('default', 'standard pricing plan', 15, 3, 10, 50, 50, 100);
