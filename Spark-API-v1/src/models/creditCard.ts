2cb57ce044ccca92eaa56ad18b5c183e45c7475f
fridavbg <fperssontech@gmail.com>
2023-01-05 13:08:06 +0100

    Finished creditCard route

--- Spark-API-v1/src/models/creditCard.ts
+++ Spark-API-v1/src/models/creditCard.ts
@@ -7,6 +7,40 @@ const saltRounds = 10;
 
 const creditCardModel = {
     /**
+     * Function to get one credit card by its id
+     * @async
+     * @returns {RowDataPacket} Resultset from the query.
+     */
+    getCard: async function getCard(cardId: string, res: Response, next: NextFunction) {
+        const db = await database.getDb();
+        try {
+            const sql = `CALL get_card(?)`;
+            const dbRes: [RowDataPacket[], FieldPacket[]] = await db.query(sql, [cardId]);
+            return res.status(200).send({ success: true, data: dbRes[0][0] });
+        } catch (error: any) {
+            next(error);
+        } finally {
+            await db.end();
+        }
+    },
+    /**
+     * Function to get all cards registered by a user
+     * @async
+     * @returns {RowDataPacket} Resultset from the query.
+     */
+    getCardsByUser: async function getCardsByUser(userId: string, res: Response, next: NextFunction) {
+        const db = await database.getDb();
+        try {
+            const sql = `CALL get_cards_by_user(?)`;
+            const dbRes: [RowDataPacket[], FieldPacket[]] = await db.query(sql, [userId]);
+            return res.status(200).send({ success: true, data: dbRes[0][0] });
+        } catch (error: any) {
+            next(error);
+        } finally {
+            await db.end();
+        }
+    },
+    /**
      * Function to save a credit card in the database
      * @async
      * @returns {RowDataPacket} Resultset from the query.
@@ -41,6 +75,23 @@ const creditCardModel = {
                 await db.end();
             }
         });
+    },
+    /**
+     * Function to delete one credit card by its id
+     * @async
+     * @returns {RowDataPacket} Resultset from the query.
+     */
+    deleteCard: async function deleteCard(cardId: string, res: Response, next: NextFunction) {
+        const db = await database.getDb();
+        try {
+            const sql = `CALL delete_card(?)`;
+            const dbRes: [RowDataPacket[], FieldPacket[]] = await db.query(sql, [cardId]);
+            return res.status(200).send({ success: true, msg: `Card with id ${cardId} has been deleted` });
+        } catch (error: any) {
+            next(error);
+        } finally {
+            await db.end();
+        }
     },
 };
 
--- Spark-API-v1/src/versions/v1/routes/creditCard.ts
+++ Spark-API-v1/src/versions/v1/routes/creditCard.ts
@@ -14,6 +14,48 @@ interface creditCardInfo {
 
 /**
  * creditCard ROUTE
+ * /creditCard/:id:
+ *   get:
+ *     summary: Get one credit card
+ *     description: Displays one credit card
+ * @param {Request}  req  The incoming request.
+ * @param {Response} res  The outgoing response.
+ * @param {Function} next Next to call in chain of middleware.
+ *
+ * @returns {void}
+ */
+router.get('/creditCard/:id', async (req: Request, res: Response, next: NextFunction) => {
+    let userId = req.params.id;
+    try {
+        return creditCardModel.getCardsByUser(userId, res, next);
+    } catch (error) {
+        next(error);
+    }
+});
+
+/**
+ * creditCard ROUTE
+ * /creditCard/:id:
+ *   get:
+ *     summary: Get one credit card by id
+ *     description: Displays one credit card for a user
+ * @param {Request}  req  The incoming request.
+ * @param {Response} res  The outgoing response.
+ * @param {Function} next Next to call in chain of middleware.
+ *
+ * @returns {void}
+ */
+router.get('/creditCard/card/:id', async (req: Request, res: Response, next: NextFunction) => {
+    let cardId = req.params.id;
+    try {
+        return creditCardModel.getCard(cardId, res, next);
+    } catch (error) {
+        next(error);
+    }
+});
+
+/**
+ * creditCard ROUTE
  * /creditCard:
  *   post:
  *     summary: Create a creditCard
@@ -33,5 +75,27 @@ router.post('/creditCard/:id', async (req: Request, res: Response, next: NextFun
         next(error);
     }
 });
+
+/**
+ * creditCard ROUTE
+ * /creditCard/:id:
+ *   delete:
+ *     summary: Delete one credit card
+ *     description: Delete one credit card
+ * @param {Request}  req  The incoming request.
+ * @param {Response} res  The outgoing response.
+ * @param {Function} next Next to call in chain of middleware.
+ *
+ * @returns {void}
+ */
+router.delete('/creditCard/:id', async (req: Request, res: Response, next: NextFunction) => {
+    let userId = req.params.id;
+    try {
+        return creditCardModel.deleteCard(userId, res, next);
+    } catch (error) {
+        next(error);
+    }
+});
+
 
 module.exports = router;
